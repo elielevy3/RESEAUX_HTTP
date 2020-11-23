04f117bedc825f8748cca95fc1d6de9020474fff
Elie3 <elie.levy.3@gmail.com>
2020-11-23 17:50:06 +0100

    adding POST, PUT, DELETE and HEAD method

--- TP-HTTP-Code/TP-HTTP-Code/src/http/server/WebServer.java
+++ TP-HTTP-Code/TP-HTTP-Code/src/http/server/WebServer.java
@@ -2,6 +2,8 @@
 
 package http.server;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,130 +12,247 @@ import java.util.ArrayList;
 /**
  * Example program from Chapter 1 Programming Spiders, Bots and Aggregators in
  * Java Copyright 2001 by Jeff Heaton
- * 
+ * <p>
  * WebServer is a very simple web-server. Any request is responded with a very
  * simple web-page.
- * 
+ *
  * @author Jeff Heaton
  * @version 1.0
  */
 public class WebServer {
 
-  /**
-   * WebServer constructor.
-   */
-  protected void start() {
-    ServerSocket s;
+    private String baseName = "/home/elie/Documents/INSA/4IF/S1/RESEAUX/HTTP/RESEAUX_HTTP/TP-HTTP-Code/TP-HTTP-Code/src/http/resources";
 
-    System.out.println("Webserver starting up on port 8080");
-    System.out.println("(press ctrl-c to exit)");
-    try {
-      // create the main server socket
-      s = new ServerSocket(8080);
-    } catch (Exception e) {
-      System.out.println("Error: " + e);
-      return;
-    }
+    /**
+     * WebServer constructor.
+     */
+    protected void start() {
+        ServerSocket s;
 
-    System.out.println("Waiting for connection");
-    for (; ; ) {
-      try {
-        // wait for a connection
-        Socket remote = s.accept();
-        // remote is now the connected socket
-        System.out.println("Connection, sending data.");
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                remote.getInputStream()));
-        PrintWriter out = new PrintWriter(remote.getOutputStream());
-
-        // read the data sent. We basically ignore it,
-        // stop reading once a blank line is hit. This
-        // blank line signals the end of the client HTTP
-        // headers.
-        String request = ".";
-        while (request != null && !request.equals("")) {
-          request = in.readLine();
-          if (request != null) {
-            String[] requestElements = request.split(" ");
-            for(String e: requestElements){
-              System.out.println(e);
-            }
-            if (requestElements.length >= 2) {
-              String action = requestElements[0];
-              String resourceName = requestElements[1];
-              if (action.equals("GET")) {
-                    String baseName = "/home/elie/Documents/INSA/4IF/S1/RESEAUX/HTTP/RESEAUX_HTTP/TP-HTTP-Code/TP-HTTP-Code/src/http/resources";
-                    ArrayList<String> response = this.fromFileToString(baseName+resourceName);
-                    out.println("HTTP/1.0 "+response.get(0));
-                    out.println("Content-Type: text/html");
-                    out.println("Server: Bot");
-                    out.println("");
-                    out.println(response.get(1));
-              }
-              else if (action.equals("POST")) {
-
-              }
-              else if (action.equals("PUT")) {
-
-              }
-              else if (action.equals("DELETE")) {
-
-              }
-              else if (action.equals("HEAD")) {
-
-              }
-              else {
-                  out.println("HTTP/1.0 400 Bad Request");
-                  out.println("Content-Type: text/html");
-                  out.println("Server: Bot");
-                  out.println("");
-                  out.println("400 Bad Request");
-              }
-            }
-            out.flush();
-            remote.close();
-          }
+        System.out.println("Webserver starting up on port 8080");
+        System.out.println("(press ctrl-c to exit)");
+        try {
+            // create the main server socket
+            s = new ServerSocket(8080);
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+            return;
         }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 
-  /**
-   * Start the application.
-   *
-   * @param args Command line parameters are not used.
-   */
-  public static void main(String args[]) {
-    WebServer ws = new WebServer();
-    ws.start();
-  }
+        System.out.println("Waiting for connection");
+        for (; ; ) {
+            try {
+                // wait for a connection
+                Socket remote = s.accept();
+                // remote is now the connected socket
+                System.out.println("Connection, sending data.");
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        remote.getInputStream()));
+                PrintWriter out = new PrintWriter(remote.getOutputStream());
+
+                // read the data sent. We basically ignore it,
+                // stop reading once a blank line is hit. This
+                // blank line signals the end of the client HTTP
+                // headers.
+                String request = ".";
+                while (request != null && !request.equals("")) {
+                    request = in.readLine();
+                    if (request != null) {
+                        String[] requestElements = request.split(" ");
+                        if (requestElements.length >= 2) {
+                            // String baseName = "/home/elie/Documents/INSA/4IF/S1/RESEAUX/HTTP/RESEAUX_HTTP/TP-HTTP-Code/TP-HTTP-Code/src/http/resources";
+                            String action = requestElements[0];
+                            String resourceName = requestElements[1];
+                            if (action.equals("GET")) {
+                                ArrayList<String> response = this.fromFileToString(baseName + resourceName);
+                                out.println("HTTP/1.0 " + response.get(0));
+                                out.println("Content-Type: text/html");
+                                out.println("Server: Bot");
+                                out.println("");
+                                out.println(response.get(1));
+                            }
+                            else if (action.equals("POST") && resourceName.split("[?]").length == 2) {
+                                String[] fileNamePlusParams = resourceName.split("[?]");
+                                // on check s'il a bien des params dans l'url
+                                String fileContentToBePosted = "";
+                                if (fileNamePlusParams.length == 2){
+                                    String fileNameToBeCreated = fileNamePlusParams[0];
+                                    String[] params = fileNamePlusParams[1].split("&");
+                                    for (String p: params){
+                                        String[] keyValuePair = p.split("=");
+                                        // si le couple key value est mal formé, il ne sera pas écrit dans le fichier
+                                        if (keyValuePair.length == 2){
+                                            String key = keyValuePair[0];
+                                            String value = keyValuePair[1];
+                                            fileContentToBePosted += key+" : "+value+"\n";
+                                        }
+                                    }
+                                    File file = new File(baseName+fileNameToBeCreated);
+                                    if (file.createNewFile()) {
+                                        System.out.println("File "+fileNameToBeCreated+" created");
+                                        FileWriter myWriter = new FileWriter(baseName+fileNameToBeCreated);
+                                        myWriter.write(fileContentToBePosted);
+                                        myWriter.close();
+                                        out.println("HTTP/1.0 200 OK");
+                                        out.println("Content-Type: text/html");
+                                        out.println("Server: Bot");
+                                        out.println("");
+                                        out.println("File"+fileNameToBeCreated+" Created and filled");
+                                    }
+                                    else{
+                                        System.out.println("File "+fileNameToBeCreated+" already exists");
+                                        // FileWriter myWriter = new FileWriter(baseName+fileNameToBeCreated);
+                                        // myWriter.write(fileContentToBePosted);
+                                        // myWriter.close();
+                                        out.println("HTTP/1.0 409 Already existing resource");
+                                        out.println("Content-Type: text/html");
+                                        out.println("Server: Bot");
+                                        out.println("");
+                                        out.println(fileNameToBeCreated+" Already exists");
+                                    }
+                                }
+                            } else if (action.equals("PUT") && resourceName.split("[?]").length == 2) {
+                                String[] fileNamePlusParams = resourceName.split("[?]");
+                                String fileContentToBePosted = "";
+                                if (fileNamePlusParams.length == 2){
+                                    String fileNameToBeCreated = fileNamePlusParams[0];
+                                    String[] params = fileNamePlusParams[1].split("&");
+                                    for (String p: params){
+                                        String[] keyValuePair = p.split("=");
+                                        // si le couple key value est mal formé, il ne sera pas écrit dans le fichier
+                                        if (keyValuePair.length == 2){
+                                            String key = keyValuePair[0];
+                                            String value = keyValuePair[1];
+                                            fileContentToBePosted += key+" : "+value+"\n";
+                                        }
+                                    }
+                                    File file = new File(baseName+fileNameToBeCreated);
+                                    FileWriter myWriter = new FileWriter(baseName+fileNameToBeCreated);
+                                    ArrayList<String> a = fromFileToString(baseName+fileNameToBeCreated);
+                                    String content = a.get(1);
+                                    System.out.println("*******");
+                                    System.out.println(baseName+fileNameToBeCreated);
+                                    System.out.println(content);
+                                    System.out.println("*******");
+                                    if (file.exists() && !file.isDirectory()) {
+                                        /** System.out.println("File "+fileNameToBeCreated+" found!");
+                                        FileWriter myWriter = new FileWriter(baseName+fileNameToBeCreated);
+                                        ArrayList<String> a = fromFileToString(baseName+fileNameToBeCreated);
+                                        String content = a.get(1);
+                                        System.out.println("*******");
+                                        System.out.println(content);
+                                        System.out.println("*******");
+                                        myWriter.append(fileContentToBePosted);
+                                        myWriter.close(); **/
+                                        out.println("HTTP/1.0 200 OK");
+                                        out.println("Content-Type: text/html");
+                                        out.println("Server: Bot");
+                                        out.println("");
+                                        out.println(fileNameToBeCreated+" updated");
+                                    }
+                                    else{
+                                        System.out.println("File "+fileNameToBeCreated+" not found");
+                                        out.println("HTTP/1.0 404 Not Found");
+                                        out.println("Content-Type: text/html");
+                                        out.println("Server: Bot");
+                                        out.println("");
+                                        out.println("File not found");
+                                    }
+                                }
+                            } else if (action.equals("DELETE")) {
+                                File file = new File(baseName+resourceName);
+                                if(file.exists() && !file.isDirectory()){
+                                    System.out.println("File "+resourceName+" found and about to be deleted");
+                                    String content = "";
+                                    if (file.delete()){
+                                        System.out.println("File "+resourceName+" deleted");
+                                        out.println("HTTP/1.0 200 OK");
+                                        content = "File "+resourceName+" deleted";
+                                    }
+                                    else{
+                                        out.println("HTTP/1.0 500 Error");
+                                        content = "Server Error occured";
+                                    }
+                                    out.println("Content-Type: text/html");
+                                    out.println("Server: Bot");
+                                    out.println("");
+                                    out.println(content);
+                                }
+                                else{
+                                    System.out.println("File "+resourceName+" not found");
+                                    out.println("HTTP/1.0 404 Not Found");
+                                    out.println("Content-Type: text/html");
+                                    out.println("Server: Bot");
+                                    out.println("");
+                                    out.println("File not found");
+                                }
+
+                            } else if (action.equals("HEAD")) {
+                                ArrayList<String> response = this.fromFileToString(baseName + resourceName);
+                                out.println("HTTP/1.0 " + response.get(0));
+                                out.println("Content-Type: text/html");
+                                out.println("Server: Bot");
+                                out.println("");
+                                //out.println(response.get(1));
+                            } else {
+                                out.println("HTTP/1.0 400 Bad Request");
+                                out.println("Content-Type: text/html");
+                                out.println("Server: Bot");
+                                out.println("");
+                                out.println("400 Bad Request");
+                            }
+                        }
+                        out.flush();
+                        remote.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Start the application.
+     *
+     * @param args Command line parameters are not used.
+     */
+    public static void main(String args[]) {
+        WebServer ws = new WebServer();
+        ws.start();
+    }
 
 
-  private ArrayList<String> fromFileToString(String path) {
-    BufferedReader reader = null;
-    ArrayList<String> response = new ArrayList<String>();
-    try{
-      reader = new BufferedReader(new FileReader(path));
-      StringBuilder stringBuilder = new StringBuilder();
-      String line = null;
-      String ls = System.getProperty("line.separator");
-      while ((line = reader.readLine()) != null) {
-        stringBuilder.append(line);
-        stringBuilder.append(ls);
-      }
-      stringBuilder.deleteCharAt(stringBuilder.length() - 1);
-      String content = stringBuilder.toString();
-      reader.close();
-      response.add("200 OK");
-      response.add(content);
+    private ArrayList<String> fromFileToString(String path) {
+        BufferedReader reader = null;
+        ArrayList<String> response = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = null;
+            String ls = System.getProperty("line.separator");
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            String content = "";
+            if (stringBuilder.length() > 0){
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+                content = stringBuilder.toString();
+                reader.close();
+            }
+            else{
+                System.out.println("LA");
+                content = "";
+            }
+            response.add("200 OK");
+            response.add(content);
+
+        } catch (IOException e) {
+            response.add("404 Not found / File error");
+            response.add("File Not Found / File Error");
+        }
+        return response;
     }
-    catch(IOException e){
-      response.add("404 Not found / File error");
-      response.add("File Not Found / File Error");
-    }
-    return response;
-  }
 }
 
